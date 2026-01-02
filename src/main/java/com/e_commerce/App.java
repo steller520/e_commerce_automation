86b43b173fde50aecac6e8542bd0436c11566f1a
steller520 <shivamnegi571@gmail.com>
2026-01-02 17:33:05 +0530

    Initialize project structure with POM, main application, and unit test

---
+++ src/main/java/com/e_commerce/App.java
@@ -0,0 +1,13 @@
+package com.e_commerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
